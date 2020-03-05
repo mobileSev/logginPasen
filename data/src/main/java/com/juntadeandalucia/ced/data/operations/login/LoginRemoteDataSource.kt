e87d390963f5f359f4e247854525c682cae7794a
Antonio Cortegoso <acortego@everis.com>
2020-03-05 09:04:52 +0100

    Fix in ClearTraffic

--- data/src/main/java/com/juntadeandalucia/ced/data/operations/login/LoginRemoteDataSource.kt
+++ data/src/main/java/com/juntadeandalucia/ced/data/operations/login/LoginRemoteDataSource.kt
@@ -5,13 +5,24 @@ import com.juntadeandalucia.ced.data.remote.ParsedResponse
 import com.juntadeandalucia.ced.data.remote.ResponseParse
 import com.juntadeandalucia.ced.domain.operations.login.CheckLoginError
 import com.juntadeandalucia.ced.domain.operations.login.LoginError
+import okhttp3.Response
+import okhttp3.ResponseBody
+import java.lang.Exception
 
 class LoginRemoteDataSource(val service : LoginService, val parser : ResponseParse) {
 
     suspend fun doLogin(request : LoginRequest) : ParsedResponse<LoginError, NoContentResponse>{
+        try {
+            val loginResponse =
+                service.doLogin(request.username, request.password, request.version).await()
+
+            return parser.parse(loginResponse)
+        }catch (e : Exception){
+            print(e)
+        }
+
         val loginResponse =
             service.doLogin(request.username, request.password, request.version).await()
-
         return parser.parse(loginResponse)
     }
 
